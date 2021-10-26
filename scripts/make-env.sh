3e319f716aed0d6fea08271e6bcadb34409d3e6f
Tong Zhao <tongzhao997@gmail.com>
2021-10-26 04:44:40 -0400

    added env creation script

---
+++ scripts/make-env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# refresh environment
+
+cd ..
+
+if [[ -d "env" ]]; then
+    rm -r env
+fi
+
+python3.10 -m venv env
+source env/bin/activate
+
+python3 -m pip install --upgrade pip
+
+# install development packages
+pip3 install mypy
+pip3 install isort
+pip3 install black
+pip3 install pylint
+
+# install src
+pip3 install -e .
+
